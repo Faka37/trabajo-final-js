89968dbf0151bd10668810109dccc30d4cea6076
Facundo Rodriguez <facundo-grodriguez@hotmail.com.ar>
2022-09-06 12:03:25 -0300

    reacomodando codigo

--- js/funciones.js
+++ js/funciones.js
@@ -1,9 +1,10 @@
 const agregandoProducto = () => {
-    let nuevoNombre = prompt("ingrese nombre")
-    let nuevaMarca = prompt("ingrese una marca")
+    let nuevoNombre = prompt("ingrese nombre").toLocaleLowerCase()
+    let nuevaMarca = prompt("ingrese una marca").toLocaleLowerCase()
     let nuevoCodigo = prompt("ingrese el  codigo")
     let nuevoPrecio = Number(prompt("ingrese el precio"))
     let nuevaDisponibilidad = confirm("ingrese si está disponible")
+
 
     let nuevoProducto = new Producto(nuevoNombre, nuevaMarca, nuevoCodigo, nuevoPrecio, nuevaDisponibilidad)
 
@@ -16,5 +17,32 @@ while (cargarProducto === true) {
     agregandoProducto()
     cargarProducto = confirm("¿Quiere cargar otro producto?")
 }
+
+function menorPrecio(){
+    productos.sort((a, b) => {
+        if (a.precio > b.precio){
+            return 1
+        }
+        if (a.precio < b.precio){
+            return -1
+        }
+        return 0 
+    })
+}
+
+function mayorPrecio(){
+    productos.sort((a, b) => {
+        if (a.precio > b.precio){
+            return -1
+        }
+        if (a.precio < b.precio){
+            return 1
+        }
+        return 0 
+    })
+}
+
+
+
 /* console.log("Productos nuevos")
 console.table(productos) */
--- js/metodos.js
+++ js/metodos.js
@@ -1,4 +1,9 @@
 //filtros 
 let disponibles = productos.filter(producto=>producto.disponibilidad===true)
+
 let noDisponibles = productos.filter(producto=>producto.disponibilidad===false)
+
 let topeMil = productos.filter(producto => producto.precio <= 1000)
+
+
+
